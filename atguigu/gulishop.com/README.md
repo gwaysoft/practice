0474f59e6371f69229aa9e9201e464541972a4d0
gwaysoft <gwaysoft@outlook.com>
2021-01-06 15:03:54 +0800

    c

--- atguigu/gulishop.com/README.md
+++ atguigu/gulishop.com/README.md
@@ -2,4 +2,4 @@
 ```text
 https://www.bilibili.com/video/BV1pE411w7zW
 ```
-![image text](./pictures/infrastruction_overview.png)
+![image text](pictures/infrastruction_overview.png)
---
+++ practice.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1609902289686" ID="ID_415705772" MODIFIED="1609902298322" TEXT="practice"/>
+</map>
